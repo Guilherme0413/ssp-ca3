363b04bdbd0e42fa3f2a2c55de1138c6da40c792
Guilherme Oliveira <oliveiraguilhermesp@gmail.com>
2022-01-06 16:27:18 +0000

    js adjusts to calculate the order

--- script.js
+++ script.js
@@ -17,10 +17,18 @@ router.get('/', function(req, res){
     let xml = fs.readFileSync('index.xml', 'utf8'),
         xsl = fs.readFileSync('index.xsl', 'utf8');
 
+    console.log(xml),
+    console.log(xsl);
+
     let doc = xmlParse(xml),
         stylesheet = xmlParse(xsl);
+
+    console.log(doc),
+    console.log(stylesheet);    
         
     let result = xsltProcess(doc, stylesheet);
+
+    console.log(result);
     
     res.end(result.toString());
     
