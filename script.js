fef10366cd9a76d7ebb243f4cf38e6f4ad7b7951
Guilherme Oliveira <oliveiraguilhermesp@gmail.com>
2022-01-06 10:35:48 +0000

    first prep

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Ciao!";
-  }
-
-  function confirmInput() {
-    fname = document.forms[0].fname.value;
-    alert("Your request was successfully submitted " + fname + "! Check your email for more details");
-  }
+const   http = require('http'), //this module provides the http server functionalities
+        path = require('path'), //path module provides utilities for working with file and directory paths
+        express = require('express'),//this allows the app to respond to https requests 
+        fs = require('fs'),//this module allows to work with the file system: read and wright files
+        xmlParse = require('xslt-processor').xmlParse,//this module allows to work with xml files
+        xsltProcess = require('xslt-processor').xsltProcess,//the same module allows to utilise xsl transformations
+        xml2js = require('xml2js') //this module does xml <-> json convertion
